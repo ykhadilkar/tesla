e3046e4524052924bda096080db9a264f406eb21
Jihad MOTII <jihad.motii@reisystems.com>
2015-06-18 14:38:15 -0400

    Readjusting repo architecture

---
+++ application/frontend/app/scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name teslaFrontApp
+ * @description
+ * # teslaFrontApp
+ *
+ * Main module of the application.
+ */
+angular
+    .module('teslaFrontApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .when('/contact', {
+                templateUrl: 'views/contact.html',
+                controller: 'ContactCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
---
+++ application/frontend/app/scripts/controllers/contact.js
@@ -0,0 +1,20 @@
+/**
+ * Created by ykhadilkar on 6/18/15.
+ */
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name teslaFrontApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the teslaFrontApp
+ */
+angular.module('teslaFrontApp')
+    .controller('ContactCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
