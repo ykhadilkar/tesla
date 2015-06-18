973fbe78f09668141d0961840267c6e8ab677949
ykhadilkar <ykhadilkar@reisys.com>
2015-06-18 10:21:35 -0400

    First commit

---
+++ client/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name teslaFrontApp
+ * @description
+ * # teslaFrontApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('teslaFrontApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name teslaFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the teslaFrontApp
+ */
+angular.module('teslaFrontApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
