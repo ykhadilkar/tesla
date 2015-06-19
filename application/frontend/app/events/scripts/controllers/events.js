6e14269f9f33b60c20f8413d2fd5cf241cdcc688
ykhadilkar <ykhadilkar@reisys.com>
2015-06-19 12:04:04 -0400

    example ajax call

--- application/frontend/app/events/scripts/controllers/events.js
+++ application/frontend/app/events/scripts/controllers/events.js
@@ -1,14 +1,25 @@
 'use strict';
+var events_data = {}
+var model = {
+    user: "Adam"
+};
 
-angular.module('teslaApp.events', ['ngRoute'])
+var teslaApp = angular.module('teslaApp.events', ['ngRoute']);
 
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/events', {
-            templateUrl: 'events/views/events.html',
-            controller: 'EventsCtrl'
-        });
-    }])
+teslaApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/events', {
+        templateUrl: 'events/views/events.html',
+        controller: 'EventsCtrl'
+    });
+}]);
 
-    .controller('EventsCtrl', [function() {
+teslaApp.run(function ($http) {
+    $http.get("https://api.fda.gov/drug/event.json?search=receivedate:[20140101+TO+20150101]&count=receivedate").success(function (data) {
+        model.items = data;
+    });
+});
 
-    }]);
+teslaApp.controller('EventsCtrl', function ($scope) {
+    $scope.todo = model;
+    $scope.message = "Hello Controller";
+});
