b2f48af68e97b433ac0da698b073ae9b4019e345
David <david.bruley@reisys.com>
2015-06-20 16:40:58 -0400

    adding my files and folders to get the front end up and running

---
+++ application/frontend/app/assets/scripts/global.js
@@ -0,0 +1,10 @@
+$(window).load(function() {
+	// Currently for all accordions with accordion id
+	if ($('#accordion')) {
+		$('#accordion').accordion({
+			active:false,
+			collapsible: true
+
+		});
+	};
+});
