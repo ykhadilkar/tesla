dd76374b4d3b0fb217058fee0359bb401ed2f3cb
miams-REISYS <miams@reisys.com>
2015-06-20 21:08:35 -0400

    changing tests to fit circle

---
+++ application/run-tests2.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -ev
+cd ${TEST_DIR}
+
+
+cd application/frontend
+
+if [ -f bower.json ]; then
+    bower install
+fi
+
+npm install
+npm test
+
+cd application/backend
+
+if [ -f bower.json ]; then
+    bower install
+fi
+
+npm install
+npm test
+
+
+
+exit 0
+
+
