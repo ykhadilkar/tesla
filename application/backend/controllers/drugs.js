f2388baa022ef899b7970478e62e8678d6df8b1f
ykhadilkar-rei <ykhadilkar@reisys.com>
2015-06-21 18:01:33 -0400

    proxy REST api for fda

---
+++ application/backend/controllers/drugs.js
@@ -0,0 +1,40 @@
+/**
+ * Created by ykhadilkar on 6/21/15.
+ */
+"use strict";
+
+var Wreck = require('wreck');
+var drugs_search_uri = "https://api.fda.gov/drug/";
+
+function DrugsController(){};
+DrugsController.prototype = (function() {
+    return {
+        events: function find(request, reply) {
+            var event_prefix = "event.json"
+            var search = request.url.search;
+            Wreck.get(drugs_search_uri + event_prefix + search, function (err, res, payload) {
+                //TODO: Get data from ES and other sources. Return mashed up magical results.
+                reply(payload);
+            });
+        },
+        labels: function find(request, reply) {
+            var label_prefix = "label.json"
+            var search = request.url.search;
+            Wreck.get(drugs_search_uri+ label_prefix + search, function (err, res, payload) {
+                //TODO: Get data from ES and other sources. Return mashed up magical results.
+                reply(payload);
+            });
+        },
+        enforcements: function find(request, reply) {
+            var enforcement_prefix = "enforcement.json"
+            var search = request.url.search;
+            Wreck.get(drugs_search_uri+ enforcement_prefix + search, function (err, res, payload) {
+                //TODO: Get data from ES and other sources. Return mashed up magical results.
+                reply(payload);
+            });
+        }
+    }
+})();
+
+var drugsController = new DrugsController();
+module.exports = drugsController;
--- application/backend/routes.js
+++ application/backend/routes.js
@@ -1,7 +1,9 @@
 var bedwetter = require('bedwetter');
 
 var options = {maxLimit:100};
-var eventsController = require('./controllers/events');
+var drugsController = require('./controllers/drugs');
+var devicesController = require('./controllers/devices');
+var foodsController = require('./controllers/foods');
 
 exports.endpoints = [
 
@@ -11,5 +13,12 @@ exports.endpoints = [
 	{method: 'POST',path: '/test',handler:{bedwetter: {}}},
 	{method: 'PATCH',path: '/test/{id}',handler:{bedwetter: {}}},
 
-    {method: 'GET',path: '/drug/event/{search}',handler:eventsController.find}
+    {method: 'GET',path: '/drug/event.json',handler:drugsController.events},
+    {method: 'GET',path: '/drug/label.json',handler:drugsController.labels},
+    {method: 'GET',path: '/drug/enforcement.json',handler:drugsController.enforcements},
+
+    {method: 'GET',path: '/device/event.json',handler:devicesController.events},
+    {method: 'GET',path: '/device/enforcement.json',handler:devicesController.enforcements},
+
+    {method: 'GET',path: '/food/enforcement.json',handler:foodsController.enforcements}
 ];
