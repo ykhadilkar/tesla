27dec41c4e23810c27470e3153703f874b4df9b1
miams-REISYS <miams@reisys.com>
2015-06-22 11:58:49 -0400

    delete files no longer needed.

--- application/fig-install.sh
+++
@@ -1,6 +0,0 @@
-rm -f rm -f python.tcz
-rm -rf /home/docker/.local/bin/fig
-wget http://www.tinycorelinux.net/5.x/x86/tcz/python.tcz && tce-load -clsi python.tcz && rm -f python.tcz
-curl -LO https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py && sudo /usr/local/bin/python2.7 ez_setup.py && rm -f ez_setup.py
-sudo /usr/local/bin/easy_install-2.7 pip
-sudo /usr/local/bin/pip2.7 install -U fig
