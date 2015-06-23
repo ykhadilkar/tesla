bb02529384894d9a940a7f658e2e09b32401a550
ykhadilkar-rei <ykhadilkar@reisys.com>
2015-06-23 11:13:30 -0400

    adding new tests

--- application/backend/test/drug.js
+++ application/backend/test/drug.js
@@ -8,7 +8,7 @@ var lab = exports.lab = Lab.script();
 var server = require('../');
 
 lab.experiment("Basic HTTP Tests", function(done) {
-    // tests
+    // test drug events end point
     lab.test("Main endpoint /drug/event.json", function(done) {
         var options = {
             method: "GET",
@@ -16,35 +16,41 @@ lab.experiment("Basic HTTP Tests", function(done) {
         };
         // server.inject lets you simulate an http request
         server.inject(options, function(response) {
-            var result = response.result;
-            Code.expect(response.statusCode).to.equal(200);  //  Expect http response status code to be 200 ("Ok")
+            var result = JSON.parse(response.result);
+            Code.expect(response.statusCode).to.equal(200);                 //  Expect http response status code to be 200 ("Ok")
+            Code.expect(result.meta.results.total).to.be.at.least(4500000); //  Atleast 4,500,000 total records
+            //console.log();
             done();                                         // done() callback is required to end the test.
         });
     });
 
+    // test drug label end point
     lab.test("Main endpoint /drug/label.json", function(done) {
         var options = {
             method: "GET",
-            url: "/drug/event.json"
+            url: "/drug/label.json"
         };
         // server.inject lets you simulate an http request
         server.inject(options, function(response) {
-            var result = response.result;
-            Code.expect(response.statusCode).to.equal(200);  //  Expect http response status code to be 200 ("Ok")
-            done();                                         // done() callback is required to end the test.
+            var result = JSON.parse(response.result);
+            Code.expect(response.statusCode).to.equal(200);                 //  Expect http response status code to be 200 ("Ok")
+            Code.expect(result.meta.results.total).to.be.at.least(70000);   //  At least 70,000 total records
+            done();                                                         // done() callback is required to end the test.
         });
     });
 
+    // test drug enforcement end point
     lab.test("Main endpoint /drug/enforcement.json", function(done) {
         var options = {
             method: "GET",
-            url: "/drug/event.json"
+            url: "/drug/enforcement.json"
         };
         // server.inject lets you simulate an http request
         server.inject(options, function(response) {
-            var result = response.result;
-            Code.expect(response.statusCode).to.equal(200);  //  Expect http response status code to be 200 ("Ok")
-            done();                                         // done() callback is required to end the test.
+            var result = JSON.parse(response.result);
+            Code.expect(response.statusCode).to.equal(200);             //  Expect http response status code to be 200 ("Ok")
+            Code.expect(result.meta.results.total).to.be.at.least(3000);//  At least 3000 total records
+            done();                                                     // done() callback is required to end the test.
         });
     });
 })
