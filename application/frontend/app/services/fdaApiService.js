d63ffb92988110985e2ef0ca4531b951130a62cd
Chris Edwards <cedwards@reisystems.com>
2015-06-23 22:13:08 -0400

    cleanup

--- application/frontend/app/services/fdaApiService.js
+++ application/frontend/app/services/fdaApiService.js
@@ -1,112 +1,118 @@
 'use strict';
 
 TeslaApp.service('fdaApiService', ['$http', '$q', function ($http, $q) {
-  var baseUrl = 'https://api.fda.gov';
+    var baseUrl = 'https://api.fda.gov';
 
-  var apiKey = 'MQcYBmlcJ41XLzrsgZyaRGlpDwEgm80uWtvxtAUi';  //  free api key
-  var apiPrefix = '?api_key=' + apiKey;
-  //  /drug/event.json?search=headache';
+    var apiKey = 'MQcYBmlcJ41XLzrsgZyaRGlpDwEgm80uWtvxtAUi';  //  free api key
+    var apiPrefix = '?api_key=' + apiKey;
+    //  /drug/event.json?search=headache';
 
 
-  var endPoints = {
-    "apiStatus": '/status',
+    var endPoints = {
+        "apiStatus": '/status',
 
-    'drugRecall': '/drug/enforcement.json',
-    'drugEvent': '/drug/event.json',
-    'drugLabel': '/drug/label.json',
+        'drugRecall': '/drug/enforcement.json',
+        'drugEvent': '/drug/event.json',
+        'drugLabel': '/drug/label.json',
 
-    'deviceRecall': '/device/enforcement.json',
-    'deviceEvent': '/device/event.json',
+        'deviceRecall': '/device/enforcement.json',
+        'deviceEvent': '/device/event.json',
 
-    'foodRecall': '/food/enforcement.json'
-  }
+        'foodRecall': '/food/enforcement.json'
+    };
 
-  var apiCall = function (endpoint, query, callback) {
-    if ('object' == typeof query) {
-      query = query.build();
-    }
+    this.endpoints = endPoints;
 
-    var deferred = $q.defer();
-    $http.get(
-      baseUrl + endpoint + apiPrefix + query
-    ).success(function (data) {
-        deferred.resolve(data);
-      }).error(function (data, status) {
-        deferred.reject(data)
-        console.log(status + ": could not get api data. Reason: " + data);
-      });
-    return deferred.promise;
-  };
+    var apiCall = function (endpoint, query, callback) {
+        if ('object' == typeof query) {
+            query = query.build();
+        }
 
-  var fdaQueryBuilder = function () {
-    this.limit = 0;
-    this.offset = 0;
-    this.count = '';
-    this.search = '';
-    return this;
-  };
+        var deferred = $q.defer();
+        $http.get(baseUrl + endpoint + apiPrefix + query).success(function (data) {
+            deferred.resolve(data);
+        }).error(function (data, status) {
+            deferred.reject(data);
+            console.log(status + ": could not get api data. Reason: " + data);
+        });
+        return deferred.promise;
+    };
 
-  fdaQueryBuilder.prototype.setLimit = function (num) {
-    this.limit = num;
-    return this;
-  };
-  fdaQueryBuilder.prototype.setOffset = function (num) {
-    this.offset = num;
-    return this;
-  };
-  fdaQueryBuilder.prototype.setCount = function (countField) {
-    this.count = countField;
-    return this;
-  }
-  fdaQueryBuilder.prototype.setCountExact = function (countField) {
-    this.count = countField + '.exact';
-    return this;
-  }
-  fdaQueryBuilder.prototype.searchString = function (str) {
-    this.search = str;
-    return this;
-  }
-  fdaQueryBuilder.prototype.build = function () {
-    var query = '';
-    query += this.search ? '&search=' + this.search : '';
-    query += this.count ? '&count=' + this.count : '';
-    query += this.offset ? '&offset=' + this.offset : '';
-    query += this.limit ? '&limit=' + this.limit : '';
-    return query;
-  }
+    var fdaQueryBuilder = function () {
+        this.limit = 0;
+        this.offset = 0;
+        this.count = '';
+        this.search = '';
+    };
 
-  //  Drug APIs
-  this.getDrugRecall = function (query, callback) {
-    //apiCall(endPoints.drugRecall, query, callback);
-  };
-  this.getDrugEvent = function (query) {
-    return apiCall(endPoints.drugEvent, query);
-  };
-  this.getDrugLabel = function (query) {
-    return apiCall(endPoints.drugLabel, query);
-  };
+    fdaQueryBuilder.prototype.setLimit = function (num) {
+        this.limit = num;
+        return this;
+    };
 
-  //  Device APIs
-  this.getDeviceRecall = function (query, callback) {
-    //apiCall(endPoints.deviceRecall, query, callback);
-  };
-  this.getDeviceEvent = function (query, callback) {
-    //apiCall(endPoints.deviceEvent, query, callback);
-  };
+    fdaQueryBuilder.prototype.setOffset = function (num) {
+        this.offset = num;
+        return this;
+    };
 
-  //  Food APIs
-  this.getFoodRecall = function (query, callback) {
-    //apiCall(endPoints.foodRecall, query, callback);
-  };
+    fdaQueryBuilder.prototype.setCount = function (countField) {
+        this.count = countField;
+        return this;
+    };
 
-  //  FDA API status
-  this.getApiStatus = function (callback) {
-    //apiCall(endPoints.apiStatus, '', callback);
-  };
+    fdaQueryBuilder.prototype.setCountExact = function (countField) {
+        this.count = countField + '.exact';
+        return this;
+    };
 
-  this.endpoints = endPoints;
-  this.queryBuilder = function() {
-    return new fdaQueryBuilder();
-  };
+    fdaQueryBuilder.prototype.searchString = function (str) {
+        this.search = str;
+        return this;
+    };
+
+    fdaQueryBuilder.prototype.build = function () {
+        var query = '';
+        query += this.search ? '&search=' + this.search : '';
+        query += this.count ? '&count=' + this.count : '';
+        query += this.offset ? '&offset=' + this.offset : '';
+        query += this.limit ? '&limit=' + this.limit : '';
+        return query;
+    };
+
+    //  Drug APIs
+    this.getDrugRecall = function (query, callback) {
+        //apiCall(endPoints.drugRecall, query, callback);
+    };
+
+    this.getDrugEvent = function (query) {
+        return apiCall(endPoints.drugEvent, query);
+    };
+
+    this.getDrugLabel = function (query) {
+        return apiCall(endPoints.drugLabel, query);
+    };
+
+    //  Device APIs
+    this.getDeviceRecall = function (query, callback) {
+        //apiCall(endPoints.deviceRecall, query, callback);
+    };
+
+    this.getDeviceEvent = function (query, callback) {
+        //apiCall(endPoints.deviceEvent, query, callback);
+    };
+
+    //  Food APIs
+    this.getFoodRecall = function (query, callback) {
+        //apiCall(endPoints.foodRecall, query, callback);
+    };
+
+    //  FDA API status
+    this.getApiStatus = function (callback) {
+        //apiCall(endPoints.apiStatus, '', callback);
+    };
+
+    this.queryBuilder = function () {
+        return new fdaQueryBuilder();
+    };
 
 }]);
