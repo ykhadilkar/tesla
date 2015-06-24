cca98588d0125bcdf6ad796d0b1360897de6c264
Alex Perfilov <alexandr.perfilov@reisystems.com>
2015-06-24 16:36:00 -0400

    following .editorconfig

--- application/frontend/app/app.js
+++ application/frontend/app/app.js
@@ -2,38 +2,38 @@
 
 // Declare app level module which depends on views, and components
 var TeslaApp = angular.module('teslaApp', [
-    'ngRoute',
-    'ngAnimate',
-    'ngAria',
-    'ngMaterial',
-    'teslaApp.home',
-    'teslaApp.search',
-    'angulartics', 
-    'angulartics.google.analytics',
+  'ngRoute',
+  'ngAnimate',
+  'ngAria',
+  'ngMaterial',
+  'teslaApp.home',
+  'teslaApp.search',
+  'angulartics',
+  'angulartics.google.analytics'
 ]);
 //   // Slide out menu
 TeslaApp
-    .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $mdUtil, $log) {
-      $scope.toggleRight = buildToggler('right');
-      /**
-       * Build handler to open/close a SideNav; when animation finishes
-       * report completion in console
-       */
-      function buildToggler(navID) {
-        return $mdUtil.debounce(function(){
-              $mdSidenav(navID)
-                .toggle()
-                .then(function () {
-                  $log.debug("toggle " + navID + " is done");
-                });
-            },300);
-      }
-    })
-    .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-      $scope.close = function () {
-        $mdSidenav('right').close()
+  .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $mdUtil, $log) {
+    $scope.toggleRight = buildToggler('right');
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildToggler(navID) {
+      return $mdUtil.debounce(function () {
+        $mdSidenav(navID)
+          .toggle()
           .then(function () {
-            $log.debug("close RIGHT is done");
+            $log.debug("toggle " + navID + " is done");
           });
-      };
-    });
+      }, 300);
+    }
+  })
+  .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+      $mdSidenav('right').close()
+        .then(function () {
+          $log.debug("close RIGHT is done");
+        });
+    };
+  });
--- application/frontend/app/search/controllers/searchCtrl.js
+++ application/frontend/app/search/controllers/searchCtrl.js
@@ -2,55 +2,55 @@
 
 angular.module('teslaApp.search', ['ngRoute'])
 
-    .controller('SearchCtrl', ['teslaFactory', 'searchFactory', 'fdaApiService', '$scope', '$location',
-        function (teslaFactory, searchFactory, fdaApiService, $scope, $location) {
-            // When the search page is initiated, grab the symptom search term from the teslaFactory
-            $scope.factorySymptom = teslaFactory.getSymptom();
+  .controller('SearchCtrl', ['teslaFactory', 'searchFactory', 'fdaApiService', '$scope', '$location',
+    function (teslaFactory, searchFactory, fdaApiService, $scope, $location) {
+      // When the search page is initiated, grab the symptom search term from the teslaFactory
+      $scope.factorySymptom = teslaFactory.getSymptom();
 
-            // Default the form value to the previously set search symptom
-            $scope.formSymptom = $scope.factorySymptom;
-            $scope.sortResultsBySafe = true;
-            $scope.someValue = 'Safest';
-
-
-            //var searchString = 'drugindication:' + $scope.formSymptom;
-
-            // ****
-            // onSearchClick - Search button click handler from the search page
-            // ****
-            $scope.onHomeSearchClick = function () {
+      // Default the form value to the previously set search symptom
+      $scope.formSymptom = $scope.factorySymptom;
+      $scope.sortResultsBySafe = true;
+      $scope.someValue = 'Safest';
 
 
-                // Update the factory first to reflect the new search term
-                teslaFactory.setSymptom($scope.formSymptom);
-                $scope.factorySymptom = $scope.formSymptom;
+      //var searchString = 'drugindication:' + $scope.formSymptom;
 
-                $location.path('/search');
-
-            };
-
-            // ****
-            // onSearchClick - Search button click handler from the search page
-            // ****
-            $scope.onSearchClick = function () {
+      // ****
+      // onSearchClick - Search button click handler from the search page
+      // ****
+      $scope.onHomeSearchClick = function () {
 
 
-                // Update the factory first to reflect the new search term
-                teslaFactory.setSymptom($scope.formSymptom);
-                $scope.factorySymptom = $scope.formSymptom;
+        // Update the factory first to reflect the new search term
+        teslaFactory.setSymptom($scope.formSymptom);
+        $scope.factorySymptom = $scope.formSymptom;
 
-                runSearch();
-            };
+        $location.path('/search');
+
+      };
+
+      // ****
+      // onSearchClick - Search button click handler from the search page
+      // ****
+      $scope.onSearchClick = function () {
 
 
-            var runSearch = function () {
+        // Update the factory first to reflect the new search term
+        teslaFactory.setSymptom($scope.formSymptom);
+        $scope.factorySymptom = $scope.formSymptom;
 
-                if ( $scope.formSymptom ) {
-                    $scope.drugResults = searchFactory.getDrugsBySymptom($scope.formSymptom, function (results) {
-                        $scope.drugResults = results;
-                    });
-                }
+        runSearch();
+      };
 
-            };
 
-        }]);
+      var runSearch = function () {
+
+        if ($scope.formSymptom) {
+          $scope.drugResults = searchFactory.getDrugsBySymptom($scope.formSymptom, function (results) {
+            $scope.drugResults = results;
+          });
+        }
+
+      };
+
+    }]);
