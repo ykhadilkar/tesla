8285741e5a05580239bbcdd41a205951c7140bd5
Chris Edwards <cedwards@reisystems.com>
2015-06-23 22:06:16 -0400

    don't call api without search term

--- application/frontend/app/search/controllers/searchCtrl.js
+++ application/frontend/app/search/controllers/searchCtrl.js
@@ -2,56 +2,57 @@
 
 angular.module('teslaApp.search', ['ngRoute'])
 
-  .controller('SearchCtrl', ['teslaFactory', 'searchFactory', 'fdaApiService', '$scope', '$location',
-    function (teslaFactory, searchFactory, fdaApiService, $scope, $location) {
-    // When the search page is initiated, grab the symptom search term from the teslaFactory
-    $scope.factorySymptom = teslaFactory.getSymptom();
+    .controller('SearchCtrl', ['teslaFactory', 'searchFactory', 'fdaApiService', '$scope', '$location',
+        function (teslaFactory, searchFactory, fdaApiService, $scope, $location) {
+            // When the search page is initiated, grab the symptom search term from the teslaFactory
+            $scope.factorySymptom = teslaFactory.getSymptom();
 
-      console.log('SearchCtrl init');
-    // Default the form value to the previously set search symptom
-    $scope.formSymptom = $scope.factorySymptom;
-    $scope.sortResultsBySafe = true;
-    $scope.someValue = 'Safest';
-
-
-    var searchString = 'drugindication:' + $scope.formSymptom;
-
-      // ****
-      // onSearchClick - Search button click handler from the search page
-      // ****
-      $scope.onHomeSearchClick = function () {
+            console.log('SearchCtrl init');
+            // Default the form value to the previously set search symptom
+            $scope.formSymptom = $scope.factorySymptom;
+            $scope.sortResultsBySafe = true;
+            $scope.someValue = 'Safest';
 
 
-        // Update the factory first to reflect the new search term
-        teslaFactory.setSymptom($scope.formSymptom);
-        $scope.factorySymptom = $scope.formSymptom;
+            var searchString = 'drugindication:' + $scope.formSymptom;
 
-        $location.path('/search');
-
-      };
-
-    // ****
-    // onSearchClick - Search button click handler from the search page
-    // ****
-    $scope.onSearchClick = function () {
+            // ****
+            // onSearchClick - Search button click handler from the search page
+            // ****
+            $scope.onHomeSearchClick = function () {
 
 
-      // Update the factory first to reflect the new search term
-      teslaFactory.setSymptom($scope.formSymptom);
-      $scope.factorySymptom = $scope.formSymptom;
+                // Update the factory first to reflect the new search term
+                teslaFactory.setSymptom($scope.formSymptom);
+                $scope.factorySymptom = $scope.formSymptom;
 
-      runSearch();
-    };
+                $location.path('/search');
+
+            };
+
+            // ****
+            // onSearchClick - Search button click handler from the search page
+            // ****
+            $scope.onSearchClick = function () {
 
 
-      var runSearch = function () {
+                // Update the factory first to reflect the new search term
+                teslaFactory.setSymptom($scope.formSymptom);
+                $scope.factorySymptom = $scope.formSymptom;
+
+                runSearch();
+            };
 
 
-        $scope.drugResults = searchFactory.getDrugsBySymptom($scope.formSymptom, function (results) {
-          console.log('in Ctrl');
-          $scope.drugResults = results;
-          console.log($scope.drugResults);
-        });
-      };
-      runSearch();
-    }]);
+            var runSearch = function () {
+
+                if ( $scope.formSymptom ) {
+                    $scope.drugResults = searchFactory.getDrugsBySymptom($scope.formSymptom, function (results) {
+                        console.log('in Ctrl');
+                        $scope.drugResults = results;
+                        console.log($scope.drugResults);
+                    });
+                }
+            };
+
+        }]);
