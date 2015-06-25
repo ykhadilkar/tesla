d95005a56134b0917beb6b98905a25a0e59c1aa5
Chris Edwards <cedwards@reisystems.com>
2015-06-25 14:36:41 -0400

    cleanup

--- application/backend/config.js
+++ application/backend/config.js
@@ -1,9 +1,6 @@
 var Path = require('path');
 
 module.exports = {
-	fizzOptions: {
-
-    },
 	server: {
         address: "0.0.0.0",
 		port: 3000
@@ -12,12 +9,5 @@ module.exports = {
 		cors: {
 			headers :['Authorization', 'Content-Type', 'If-None-Match','If-Modified-Since']
 		}
-	},
-	logOptions : {
-        opsInterval: 1000,
-        reporters: [{
-            reporter: require('good-console'),
-            events: { log: '*', response: '*' }
-        }]
-    }
+	}
 };
