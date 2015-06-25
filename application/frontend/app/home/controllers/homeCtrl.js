ca557d721cad67959ceb599268b587dab2b192c7
Alex Perfilov <alexandr.perfilov@reisystems.com>
2015-06-25 15:23:28 -0400

    cleanup :hamburger:

--- application/frontend/app/home/controllers/homeCtrl.js
+++ application/frontend/app/home/controllers/homeCtrl.js
@@ -1,11 +1,8 @@
 'use strict';
 
 angular.module('teslaApp.home', [])
-  .controller('HomeCtrl', ['$scope', 'teslaFactory', function ($scope, teslaFactory) {
+  .controller('HomeCtrl', ['$scope', function ($scope) {
 
     $scope.symptom = "My Symptom";
     $scope.drug = "";
-
-    console.log('InHomeCtrl');
-
   }]);
