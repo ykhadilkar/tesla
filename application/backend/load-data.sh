5e4c5cbc00992681b4c27bc330fce142cee6b70b
miams-REISYS <miams@reisys.com>
2015-07-01 13:55:19 -0400

    update load synonym data variable

--- application/backend/load-data.sh
+++ application/backend/load-data.sh
@@ -1,4 +1,4 @@
 gunzip medical_index_mapping.json.gz
-elasticdump --input=medical_index_mapping.json --output=http://$TEXTDB_1_ENV_TUTUM_SERVICE_HOSTNAME:9200/medical --type=mapping 
+elasticdump --input=medical_index_mapping.json --output=http://$TEXTDB_1_PORT_9200_TCP_ADDR:9200/medical --type=mapping 
 gunzip medical_index_data.json.gz
-elasticdump --input=medical_index_data.json --output=http://$TEXTDB_1_ENV_TUTUM_SERVICE_HOSTNAME:9200/medical --type=data
+elasticdump --input=medical_index_data.json --output=http://$TEXTDB_1_PORT_9200_TCP_ADDR:9200/medical --type=data
