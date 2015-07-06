4b6418a26c18daef38694b81c3f7e5e3cad09939
CarlosValle <REI Systems>
2015-07-06 13:54:16 -0400

    Enabling angular accent classes

--- application/frontend/app/app.js
+++ application/frontend/app/app.js
@@ -9,4 +9,18 @@ var TeslaApp = angular.module('teslaApp', [
     'config',
     'angularSpinner',
     'nvd3'
-]);
+])
+.config(function($mdThemingProvider) {
+  // Extend the teal theme with different color
+  var reiTealMap = $mdThemingProvider.extendPalette('teal', {
+    'A400': '47d091'
+  });
+  // Register the new color palette map with the name
+  $mdThemingProvider.definePalette('reiTeal', reiTealMap);
+  // Use that theme for the accent intentions
+  $mdThemingProvider.theme('default')
+    .primaryPalette('indigo')
+    .accentPalette('reiTeal',{
+      'default': 'A400'
+    });
+});
