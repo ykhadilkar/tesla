eddbc3c393b733898f54a0f3ce23f442aec4e507
Alex Perfilov <alexandr.perfilov@reisystems.com>
2015-07-06 22:12:45 -0400

    backend API test :tophat:

---
+++ application/frontend/test/spec/services/backendApiServiceTest.js
@@ -0,0 +1,53 @@
+'use strict';
+
+describe('backendApiService', function () {
+    var escapeRegExp = function (str) {
+        return str.replace(/[\/\.\?]/g, "\\$&");
+    };
+
+    var $httpBackend,
+        backendApiServiceObj,
+        baseUrl;
+
+    beforeEach(function () {
+        module('teslaApp');
+        inject(function (_$httpBackend_, backendApiService, ENV) {
+            $httpBackend = _$httpBackend_;
+            backendApiServiceObj = backendApiService;
+            baseUrl = ENV.BACKEND_API;
+        })
+    });
+
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it("checks endpoints list", function () {
+        expect(backendApiServiceObj.endpoints).toEqual(jasmine.any(Object));
+        expect(backendApiServiceObj.endpoints.synonym).toEqual(jasmine.stringMatching('/symptom.json'));
+    });
+
+    it("checks functions are defined", function () {
+        expect(backendApiServiceObj.getConditionSynonyms).toEqual(jasmine.any(Function));
+        expect(backendApiServiceObj.autoComplete).toEqual(jasmine.any(Function));
+        expect(backendApiServiceObj.getProduct).toEqual(jasmine.any(Function));
+    });
+
+    xit("checks /symptom.json request", function () {
+        var apiUrl = new RegExp(
+            escapeRegExp(baseUrl + backendApiServiceObj.endpoints.synonym)
+            + 'karma', 'gi');
+
+        $httpBackend.when('GET', apiUrl).respond(
+            {drugInfo: 'some info'}
+        );
+        var status = false;
+        backendApiServiceObj.getConditionSynonyms('karma').then(function (result) {
+            status = result;
+        });
+        $httpBackend.flush();
+
+        expect(status).toEqual({drugInfo: 'some info'});
+    });
+});
