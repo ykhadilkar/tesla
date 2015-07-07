d613ebb5e27e147787fbbbeb31b7ce40ddaec3a9
JihadMotii-REISys <jihad.motii@reisystems.com>
2015-07-07 10:00:31 -0400

    change var env staging backend API

--- application/frontend/Gruntfile.js
+++ application/frontend/Gruntfile.js
@@ -65,7 +65,7 @@ module.exports = function (grunt) {
           },
           constants: {
             ENV: {
-              BACKEND_API: 'http://velocirx.reisys.com',
+              BACKEND_API: 'http://tesla-api.devopsplatform.com',
               FDA_API: 'https://api.fda.gov',
               RXNORM_API: 'http://rxnav.nlm.nih.gov/REST',
             }
